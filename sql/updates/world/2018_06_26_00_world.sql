401adb5abe82e8ccec4329137b409c88ca1c96b4
NuskyDev <luciano.mamonas98@gmail.com>
2018-07-02 04:52:59 +0000

    Remove Updates old

--- sql/updates/world/2018_06_26_00_world.sql
+++
@@ -1,7 +0,0 @@
--- Searing Totem fix by GMKyle
-
-UPDATE creature_template SET spell1=3606 WHERE entry=2523;
-
-
--- Fire Elemental Totem fix
-UPDATE creature_template SET spell1=117663 WHERE entry=15439;
--- sql/updates/world/2018_06_26_02_world.sql
+++
@@ -1,2 +0,0 @@
--- UPDATE gameobject_template is entry = 210986
-UPDATE gameobject_template SET unkInt32='0' WHERE entry='210986';
