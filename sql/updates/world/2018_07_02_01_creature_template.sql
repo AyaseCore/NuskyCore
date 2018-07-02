fff82f131c8dd0b119ebf85bc3fa14b2b344a0be
NuskyDev <luciano.mamonas98@gmail.com>
2018-07-02 05:37:42 +0000

    DB/Creature: Added Tushui Trainee

--- sql/updates/world/2018_07_02_01_creature_template.sql
+++ sql/updates/world/2018_07_02_01_creature_template.sql
@@ -1,2 +1,5 @@
 -- Spirit of Master Shang Xi
 UPDATE `creature_template` SET ScriptName='npc_master_shang_xi' WHERE entry='56193';
+
+-- Tushui Trainee
+UPDATE `creature_template` SET KillCredit1='54587', KillCredit2='65470', SriptName='npc_tushui_trainee';
